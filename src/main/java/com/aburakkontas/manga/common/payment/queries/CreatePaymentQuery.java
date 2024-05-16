c41a1b3bf7156b8e8e61b591ee099f0043fdb3d1
BurakKontas <aburakkontas@trakya.edu.tr>
2024-05-16 19:54:14 +0300

    # Added GetUserDetailsFromTokenQuery and Result

---
+++ src/main/java/com/aburakkontas/manga/common/payment/queries/CreatePaymentQuery.java
@@ -0,0 +1,9 @@
+package com.aburakkontas.manga.common.payment.queries;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CreatePaymentQuery {
+}
---
+++ src/main/java/com/aburakkontas/manga/common/payment/queries/results/CreatePaymentQueryResult.java
@@ -0,0 +1,7 @@
+package com.aburakkontas.manga.common.payment.queries.results;
+
+import lombok.Data;
+
+@Data
+public class CreatePaymentQueryResult {
+}
